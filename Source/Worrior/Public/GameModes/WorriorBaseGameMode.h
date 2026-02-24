7d1794f679708ce902ff4f4db59bd90f1e1b2dd7
kyalyal <91156168+kyalyal@users.noreply.github.com>
2026-02-24 21:55:42 +0900

    기본 캐릭터 및 컨트롤러 게임모드 제작

---
+++ Source/Worrior/Public/GameModes/WorriorBaseGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WorriorBaseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WORRIOR_API AWorriorBaseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
