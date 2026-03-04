58a1a534102c73aa4a8944e8151f69589e08d4f9
kyalyal <91156168+kyalyal@users.noreply.github.com>
2026-03-04 16:07:54 +0900

    애님 레이어를 통해 애니메이션 연결

--- Source/Warrior/Private/AnimInstance/Hero/WarriorHeroLinkedAnimLayer.cpp
+++ Source/Warrior/Private/AnimInstance/Hero/WarriorHeroLinkedAnimLayer.cpp
@@ -2,4 +2,10 @@
 
 
 #include "AnimInstance/Hero/WarriorHeroLinkedAnimLayer.h"
+#include "AnimInstance/Hero/WarriorHeroAnimInstance.h"
 
+UWarriorHeroAnimInstance* UWarriorHeroLinkedAnimLayer::GetHeroAnimInstance() const
+{
+    return Cast<UWarriorHeroAnimInstance>(GetOwningComponent()->GetAnimInstance());
+    
+}
